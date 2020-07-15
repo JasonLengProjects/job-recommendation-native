df5a56738ca5801f02fcfbec0a5dc4735502fde7
JasonLengProjects <hongze.leng@colorado.edu>
2020-07-14 22:59:55 -0600

    added keywords feature

--- src/main/java/external/GitHubClient.java
+++ src/main/java/external/GitHubClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -68,22 +69,35 @@ public class GitHubClient {
 
 	private List<Item> getItemList(JSONArray array) {
 		List<Item> itemList = new ArrayList<>();
+		List<String> descriptionList = new ArrayList<>();
+
+		for (int i = 0; i < array.length(); i++) {
+//			descriptionList.add(getStringFieldOrEmpty(array.getJSONObject(i), "title"));
+			String description = getStringFieldOrEmpty(array.getJSONObject(i), "description");
+			if (description.equals("") || description.equals("\n")) {
+				descriptionList.add(getStringFieldOrEmpty(array.getJSONObject(i), "title"));
+			} else {
+				descriptionList.add(description);
+			}
+		}
+
+		List<List<String>> keywords = MonkeyLearnClient
+				.extractKeywords(descriptionList.toArray(new String[descriptionList.size()]));
 
 		for (int i = 0; i < array.length(); i++) {
 			JSONObject object = array.getJSONObject(i);
 			Item item = Item.builder().itemId(getStringFieldOrEmpty(object, "id"))
 					.name(getStringFieldOrEmpty(object, "title")).address(getStringFieldOrEmpty(object, "location"))
 					.url(getStringFieldOrEmpty(object, "url")).imageUrl(getStringFieldOrEmpty(object, "company_logo"))
-					.build();
+					.keywords(new HashSet<String>(keywords.get(i))).build();
 			itemList.add(item);
 		}
 
 		return itemList;
 	}
-	
+
 	private String getStringFieldOrEmpty(JSONObject obj, String field) {
 		return obj.isNull(field) ? "" : obj.getString(field);
 	}
-
 
 }
